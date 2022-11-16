7999948ce1ded6a80295fe821d08a66e754dccab
jstopcoder <jstopcoder0209@gmail.com>
2022-11-16 23:35:46 +0900

    Update style

--- client/src/components/ReportPage.js
+++ client/src/components/ReportPage.js
@@ -124,91 +124,93 @@ const ReportPage = () => {
                     </div>
                 </div>
                 <div className="row pb-5">
-                    <Table striped size="sm">
-                        <thead bordered-0>
-                            <tr>
-                                <th>Region Sales Stage</th>
-                                <th>Central Opportunity Count</th>
-                                <th>Revenue</th>
-                                <th>East Opportunity Count</th>
-                                <th>Revenue</th>
-                                <th>West Opportunity Count</th>
-                                <th>Revenue</th>
-                                <th>Total Opportunity Count</th>
-                                <th>Revenue</th>
-                            </tr>
-                        </thead>
-                        <tbody bordered hover>
-                            <tr>
-                                <td className="row-title">Lead</td>
-                                <td className="text-right">91</td>
-                                <td className="text-right">$441,003,315</td>
-                                <td className="text-right">101</td>
-                                <td className="text-right">$419,536,437</td>
-                                <td className="text-right">47</td>
-                                <td className="text-right">$234,397,428</td>
-                                <td className="text-right">239</td>
-                                <td className="text-right">$1,094,967,180</td>
-                            </tr>
-                            <tr>
-                                <td className="row-title">Quality</td>
-                                <td className="text-right">129</td>
-                                <td className="text-right">$111,715,461</td>
-                                <td className="text-right">50</td>
-                                <td className="text-right">$195,692,154</td>
-                                <td className="text-right">15</td>
-                                <td className="text-right">$53,442,363</td>
-                                <td className="text-right">94</td>
-                                <td className="text-right">$359,849,978</td>
-                            </tr>
-                            <tr>
-                                <td className="row-title">Solution</td>
-                                <td className="text-right">29</td>
-                                <td className="text-right">$100,743,789</td>
-                                <td className="text-right">30</td>
-                                <td className="text-right">$134,347,170</td>
-                                <td className="text-right">15</td>
-                                <td className="text-right">$53,441,501</td>
-                                <td className="text-right">74</td>
-                                <td className="text-right">$228,532,460</td>
-                            </tr>
-                            <tr>
-                                <td className="row-title">Proposal</td>
-                                <td className="text-right">14</td>
-                                <td className="text-right">$46,722,869</td>
-                                <td className="text-right">13</td>
-                                <td className="text-right">$59,970,924</td>
-                                <td className="text-right">10</td>
-                                <td className="text-right">$43,032,669</td>
-                                <td className="text-right">37</td>
-                                <td className="text-right">$149,726,462</td>
-                            </tr>
-                            <tr>
-                                <td className="row-title">Finalize</td>
-                                <td className="text-right">5</td>
-                                <td className="text-right">$23,302,246</td>
-                                <td className="text-right">5</td>
-                                <td className="text-right">$30,696,428</td>
-                                <td className="text-right">4</td>
-                                <td className="text-right">$21,176,185</td>
-                                <td className="text-right">14</td>
-                                <td className="text-right">$75,174,859</td>
-                            </tr>
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <th className="row-title">Total</th>
-                                <th className="text-right">168</th>
-                                <th className="text-right">$723,517,680</th>
-                                <th className="text-right">199</th>
-                                <th className="text-right">$840,243,113</th>
-                                <th className="text-right">91</th>
-                                <th className="text-right">$404,490,146</th>
-                                <th className="text-right">458</th>
-                                <th className="text-right">$1,968,250,939</th>
-                            </tr>
-                        </tfoot>
-                    </Table>
+                    <div className="col-12 overflow-scroll">
+                        <Table striped size="sm">
+                            <thead bordered-0>
+                                <tr>
+                                    <th>Region Sales Stage</th>
+                                    <th>Central Opportunity Count</th>
+                                    <th>Revenue</th>
+                                    <th>East Opportunity Count</th>
+                                    <th>Revenue</th>
+                                    <th>West Opportunity Count</th>
+                                    <th>Revenue</th>
+                                    <th>Total Opportunity Count</th>
+                                    <th>Revenue</th>
+                                </tr>
+                            </thead>
+                            <tbody bordered hover>
+                                <tr>
+                                    <td className="row-title">Lead</td>
+                                    <td className="text-right">91</td>
+                                    <td className="text-right">$441,003,315</td>
+                                    <td className="text-right">101</td>
+                                    <td className="text-right">$419,536,437</td>
+                                    <td className="text-right">47</td>
+                                    <td className="text-right">$234,397,428</td>
+                                    <td className="text-right">239</td>
+                                    <td className="text-right">$1,094,967,180</td>
+                                </tr>
+                                <tr>
+                                    <td className="row-title">Quality</td>
+                                    <td className="text-right">129</td>
+                                    <td className="text-right">$111,715,461</td>
+                                    <td className="text-right">50</td>
+                                    <td className="text-right">$195,692,154</td>
+                                    <td className="text-right">15</td>
+                                    <td className="text-right">$53,442,363</td>
+                                    <td className="text-right">94</td>
+                                    <td className="text-right">$359,849,978</td>
+                                </tr>
+                                <tr>
+                                    <td className="row-title">Solution</td>
+                                    <td className="text-right">29</td>
+                                    <td className="text-right">$100,743,789</td>
+                                    <td className="text-right">30</td>
+                                    <td className="text-right">$134,347,170</td>
+                                    <td className="text-right">15</td>
+                                    <td className="text-right">$53,441,501</td>
+                                    <td className="text-right">74</td>
+                                    <td className="text-right">$228,532,460</td>
+                                </tr>
+                                <tr>
+                                    <td className="row-title">Proposal</td>
+                                    <td className="text-right">14</td>
+                                    <td className="text-right">$46,722,869</td>
+                                    <td className="text-right">13</td>
+                                    <td className="text-right">$59,970,924</td>
+                                    <td className="text-right">10</td>
+                                    <td className="text-right">$43,032,669</td>
+                                    <td className="text-right">37</td>
+                                    <td className="text-right">$149,726,462</td>
+                                </tr>
+                                <tr>
+                                    <td className="row-title">Finalize</td>
+                                    <td className="text-right">5</td>
+                                    <td className="text-right">$23,302,246</td>
+                                    <td className="text-right">5</td>
+                                    <td className="text-right">$30,696,428</td>
+                                    <td className="text-right">4</td>
+                                    <td className="text-right">$21,176,185</td>
+                                    <td className="text-right">14</td>
+                                    <td className="text-right">$75,174,859</td>
+                                </tr>
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th className="row-title">Total</th>
+                                    <th className="text-right">168</th>
+                                    <th className="text-right">$723,517,680</th>
+                                    <th className="text-right">199</th>
+                                    <th className="text-right">$840,243,113</th>
+                                    <th className="text-right">91</th>
+                                    <th className="text-right">$404,490,146</th>
+                                    <th className="text-right">458</th>
+                                    <th className="text-right">$1,968,250,939</th>
+                                </tr>
+                            </tfoot>
+                        </Table>
+                    </div>
                 </div>
             </div>
         </Fragment>
